4d2e0f4b2a81174575c16b6691de3394429cbba2
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-20 12:38:09 +0700

    data class

--- app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/modeldata/DataSiswa.kt
@@ -4,10 +4,10 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class DataSiswa(
-    val id: Int,
-    val nama: String,
-    val alamat: String,
-    val telpon: String
+    val id : Int,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
 )
 
 data class UIStateSiswa(
@@ -16,29 +16,9 @@ data class UIStateSiswa(
 )
 
 data class DetailSiswa(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val telpon: String = ""
+    val id : Int = 0,
+    val nama : String = "",
+    val alamat : String = "",
+    val telpon : String = ""
 )
 
-fun DetailSiswa.toDataSiswa(): DataSiswa = DataSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon
-)
-
-fun DataSiswa.toUIStateSiswa(
-    isEntryValid: Boolean = false
-): UIStateSiswa = UIStateSiswa(
-    detailSiswa = this.toDetailSiswa(),
-    isEntryValid = isEntryValid
-)
-
-fun DataSiswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    telpon = telpon
-)
