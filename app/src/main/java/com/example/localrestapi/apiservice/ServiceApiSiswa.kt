aa519bad0922436a570a30a4d7ba6b75a366e0c4
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-20 12:53:32 +0700

    servisApi

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
@@ -1,18 +1,14 @@
 package com.example.localrestapi.apiservice
 
-import com.example.mydatasiswa.modeldata.DataSiswa
+import com.example.localrestapi.modeldata.DataSiswa
 import retrofit2.http.Body
-import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.PUT
-import retrofit2.http.Query
 
 interface ServiceApiSiswa {
-
-    @GET(value = "bacaTeman.php")
+    @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
-    @POST(value = "insertTm.php")
-    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+    @POST("insertTM.php")
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
