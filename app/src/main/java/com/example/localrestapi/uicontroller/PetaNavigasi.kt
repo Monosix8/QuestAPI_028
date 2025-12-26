da820b4498fa887e86f9934c76a98dd1f7c1a01e
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:37:21 +0700

    menyiapkan NavController dan memanggil HostNavigasi

--- app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -3,11 +3,17 @@ package com.example.localrestapi.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.localrestapi.uicontroller.route.DestinasiDetail
+import com.example.localrestapi.uicontroller.route.DestinasiEdit
 import com.example.localrestapi.uicontroller.route.DestinasiEntry
 import com.example.localrestapi.uicontroller.route.DestinasiHome
+import com.example.localrestapi.view.DetailSiswaScreen
+import com.example.localrestapi.view.EditSiswaScreen
 import com.example.localrestapi.view.EntrySiswaScreen
 import com.example.localrestapi.view.HomeScreen
 
@@ -17,21 +23,3 @@ fun DataSiswaApp(navController: NavHostController = rememberNavController(),
     HostNavigasi(navController = navController)
 }
 
-@Composable
-fun HostNavigasi(navController: NavHostController,
-                 modifier: Modifier = Modifier) {
-    NavHost(navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = Modifier) {
-        composable(DestinasiHome.route) {
-            HomeScreen(navigateToItemEntry = {
-                navController.navigate(DestinasiEntry.route) },
-                navigateToItemUpdate = {
-                    navController.navigate("${DestinasiEntry.route}/${it}")
-                })
-        }
-        composable(DestinasiEntry.route) {
-            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route)})
-        }
-    }
-}
