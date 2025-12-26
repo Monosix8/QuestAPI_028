aa254c7af9e6d36d0d2491b04fa9bd151c2f11ba
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:22:39 +0700

    mendefinisikan aturan jalan (route) ketika aplikasi berpindah dari halaman daftar siswa ke halaman detail satu siswa

--- app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,11 @@
 package com.example.localrestapi.uicontroller.route
 
+import com.example.localrestapi.R
+
+object DestinasiDetail: DestinasiNavigasi{
+    override val route= "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
