764210499e585be9fa9a4da1928b551890ac4468
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:43:26 +0700

    ViewModel untuk mengelola proses edit data siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/EditViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.localrestapi.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localrestapi.modeldata.DetailSiswa
+import com.example.localrestapi.modeldata.UIStateSiswa
+import com.example.localrestapi.modeldata.toDataSiswa
+import com.example.localrestapi.modeldata.toUiStateSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import com.example.localrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
