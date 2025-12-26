1bcbf8f28e2e2de453df9e0e5e747e487a493356
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:29:49 +0700

    Menentukan status UI pada halaman detail siswa

--- app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/DetailViewModel.kt
@@ -1,2 +1,22 @@
 package com.example.localrestapi.viewmodel
 
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.localrestapi.modeldata.DataSiswa
+import com.example.localrestapi.repositori.RepositoryDataSiswa
+import com.example.localrestapi.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
+import retrofit2.Response
+
+sealed interface StatusUIDetail{
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
