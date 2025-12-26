9f8ef6e54c7be4ae16f66af2a8603c0d9a0ed7ae
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:18:29 +0700

    menambah fungsi untuk melihat 1 siswa

--- app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/localrestapi/apiservice/ServiceApiSiswa.kt
@@ -2,13 +2,19 @@ package com.example.localrestapi.apiservice
 
 import com.example.localrestapi.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
-    @GET("bacaTeman.php")
+    @GET("bacateman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
-}
+
+    @GET("baca1teman.php")
+    suspend fun getSatuSiswa(@Query("id") id:Int): DataSiswa
+
