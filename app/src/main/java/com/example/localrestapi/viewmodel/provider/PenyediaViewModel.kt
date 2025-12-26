486483df4bff6baaf24657438a74c81ae6e84ef7
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:46:24 +0700

    function untuk mengambil instance AplikasiDataSiswa

--- app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/localrestapi/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.localrestapi.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.localrestapi.repositori.AplikasiDataSiswa
+import com.example.localrestapi.viewmodel.DetailViewModel
+import com.example.localrestapi.viewmodel.EditViewModel
 import com.example.localrestapi.viewmodel.EntryViewModel
 import com.example.localrestapi.viewmodel.HomeViewModel
 
@@ -19,5 +22,17 @@ object PenyediaViewModel {
             .repositoriDataSiswa) }
         initializer { EntryViewModel(aplikasiDataSiswa().container
             .repositoriDataSiswa) }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoriDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoriDataSiswa
+            )
+        }
     }
 }
