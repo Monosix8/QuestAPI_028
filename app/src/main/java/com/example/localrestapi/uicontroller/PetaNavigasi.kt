dbe0703c28464023b93b103f659d71f1075f12ce
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:39:14 +0700

    mengatur seluruh navigasi antar layar aplikasi

--- app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -23,3 +23,28 @@ fun DataSiswaApp(navController: NavHostController = rememberNavController(),
     HostNavigasi(navController = navController)
 }
 
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ){
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/$it")
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+
