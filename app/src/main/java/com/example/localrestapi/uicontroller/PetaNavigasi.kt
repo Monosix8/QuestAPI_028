0f3bd94523e91169c8acbcf317b1bd3a7917cde5
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:40:14 +0700

    Navigasi ke halaman Detail siswa dengan parameter ID

--- app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/localrestapi/uicontroller/PetaNavigasi.kt
@@ -48,3 +48,17 @@ fun HostNavigasi(
             )
         }
 
+        composable(
+            DestinasiDetail.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetail.itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateToEditItem = {
+                    navController.navigate("${DestinasiEdit.route}/$it")
+                },
+                navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+
