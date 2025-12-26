2c31395ba356e2af7a0fee2136137adbefae193f
Monosix8 <ridhohidayatullah008@gmail.com>
2025-12-27 01:35:12 +0700

    Interface repository untuk mengatur akses data siswa

--- app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
+++ app/src/main/java/com/example/localrestapi/repositori/RepositoriDataSiswa.kt
@@ -6,12 +6,9 @@ import com.example.localrestapi.modeldata.DataSiswa
 interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
     suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id:Int, dataSiswa:DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id:Int): retrofit2.Response<Void>
 }
 
-class JaringanRepositoryDataSiswa(
-    private val serviceApiSiswa: ServiceApiSiswa
-) : RepositoryDataSiswa {
 
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-}
